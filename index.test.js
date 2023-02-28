6bf33fc213203c9378801624c9362baf105af71f
Marcy Sutton <holla@marcysutton.com>
2023-02-28 14:57:10 -0800

    Get test working with vanilla HTML and TestingLib

--- index.test.js
+++ index.test.js
@@ -1,26 +1,38 @@
 import prettify from "prettify"
 import { JSDOM } from "jsdom"
-import { screen } from "@testing-library/dom"
+import { getByText, getByTestId, getByRole } from "@testing-library/dom"
 import '@testing-library/jest-dom/extend-expect'
 import userEvent from '@testing-library/user-event'
 
-const user = userEvent.setup()
+let user
 let document
 let container
 
 describe('Editable title', () => {
   beforeEach(() => {
-    return JSDOM.fromFile('./index.html')
-    .then((dom) => {
-      document = dom.window.document
-      container = dom.window.document.querySelector('main')
-    })
+    const options = {
+      resources: 'usable',
+      runScripts: 'dangerously',
+    }
+    return JSDOM.fromFile('./index.html', options)
+      .then((dom) => {
+        document = dom.window.document
+        container = dom.window.document.querySelector('main')
+
+        // tell userEvent to use loaded DOM
+        // necessary to use .tab()
+        user = userEvent.setup({document: document})
+      })
   })
 
-  it('has a focusable element', () => {
-    user.tab()
-    console.log(container.innerHTML)
-    const editHandle = screen.queryByTestId('edit-handle')
+  it('has a focusable element', async () => {
+    const button = getByRole(container, 'button')
+    await button.focus()
+    expect(button).toHaveFocus()
+
+    await user.tab()
+
+    const editHandle = getByTestId(container, 'edit-handle')
     expect(editHandle).toHaveFocus()
   })
 })
