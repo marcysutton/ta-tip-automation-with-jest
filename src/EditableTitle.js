91491f06869b750f813e90d86c47b9cab4b7b25a
Marcy Sutton <holla@marcysutton.com>
2023-03-10 12:44:25 -0800

    Update React component version

--- src/EditableTitle.js
+++ src/EditableTitle.js
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
 const EditableTitle = () => {
+    const [isEditing, setIsEditing] = useState(false)
+
     return (
-        <>
+        <h3 data-testid="editable-title"
+            className={isEditing ? 'editing' : null}
+        >
             <a 
-                href="https://codepen.io/marcysutton/pen/JjBqzxx" 
-                className="ItemTitle_titleLink-dPGjg"
+                href="https://codepen.io/marcysutton/pen/JjBqzxx"
+                contentEditable={isEditing ? 'true' : 'false'}
+                className="ItemTitle_titleLink"
                 id="editable-title-span"
-                data-testid="edit-title-link"
+                data-testid="editable-title-link"
                 title="Testing Accessibility Tips: Accessibility TDD"
             >
                 Testing Accessibility Tips: Accessibility TDD
             </a>
-            <svg
-                className="ItemTitle_iconEdit-rW2EJ"
-                data-testid="edit-handle"
-                viewBox="0 0 100 100" 
+            <span
+                data-testid="editable-handle"
+                onClick={()=> { setIsEditing(!isEditing) }}
             >
-                <path d="M24.56 92.536 0 100l7.453-24.583c6.356-.244 17.322 10.792 17.107 17.119zM96.617 32.082l-.475.471L67.383 3.766l.472-.472c12.927-12.94 42.016 15.517 28.762 28.788zM61.64 9.516l28.758 28.785-46.303 46.345c-1.222 1.221-2.234.884-2.234.884l2.314-15.356-14.454.074.072-14.468-15.342 2.312s-.34-1.011.883-2.234L61.64 9.516z"></path>
-            </svg>
-        </>
+                <svg
+                    className="ItemTitle_iconEdit"
+                    viewBox="0 0 100 100"
+                >
+                    <path d="M24.56 92.536 0 100l7.453-24.583c6.356-.244 17.322 10.792 17.107 17.119zM96.617 32.082l-.475.471L67.383 3.766l.472-.472c12.927-12.94 42.016 15.517 28.762 28.788zM61.64 9.516l28.758 28.785-46.303 46.345c-1.222 1.221-2.234.884-2.234.884l2.314-15.356-14.454.074.072-14.468-15.342 2.312s-.34-1.011.883-2.234L61.64 9.516z"></path>
+                </svg>
+            </span>
+        </h3>
     )
 }
 
--- src/EditableTitle.test.js
+++ src/EditableTitle.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, getByTestId, getByRole} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event'
 
@@ -10,14 +10,28 @@ const user = userEvent.setup()
 describe('EditableTitle-React', () => {
     it('renders', () => {
         render(<EditableTitle />)
-        const example = screen.getByTestId('edit-handle')
+        const example = screen.getByTestId('editable-handle')
         expect(example).toBeInTheDocument()
     })
-    it('has a focusable element', async () => {
+    xit('has an accessibly interactive element', async () => {
         render(<EditableTitle />)
         await user.tab()
         
-        const editHandle = screen.getByTestId('edit-handle')
+        const editableTitle = screen.getByTestId('editable-title')
+        const editHandle = getByRole(editableTitle, 'button')
         expect(editHandle).toHaveFocus()
     })
+    it('starts editing', async () => {
+        render(<EditableTitle />)
+        const editHandle = screen.getByTestId('editable-handle')
+        editHandle.focus()
+
+        const editableTitle = screen.getByTestId('editable-title-link')
+
+        await editHandle.click()
+
+        // await needed to allow time for attribute to flip
+        await expect(editableTitle).toHaveAttribute('contenteditable', 'true')
+        expect(editableTitle).toHaveFocus()
+      })
 })
